51800e40f31109b0559e0bd0f5ec946ed7aee21e
Marcoto06 <heroeoftime06@gmail.com>
2026-04-15 12:07:46 +0200

    map

---
+++ PlatformGame-L19_Optimization/Assets/Maps/Fondo Boca.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Fondo Boca" tilewidth="64" tileheight="64" tilecount="1980" columns="60">
+ <image source="Fondo Boca.png" width="3840" height="2160"/>
+</tileset>
