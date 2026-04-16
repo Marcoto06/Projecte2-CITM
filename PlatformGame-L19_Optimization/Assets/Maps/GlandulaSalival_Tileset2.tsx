8766f917c4407d8291cc12a507674f70aaab5068
Marc <heroeoftime06@gmail.com>
2026-04-16 17:30:30 +0200

    map

--- PlatformGame-L19_Optimization/Assets/Maps/GlandulaSalival_Tileset2.tsx
+++ PlatformGame-L19_Optimization/Assets/Maps/GlandulaSalival_Tileset2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="GlandulaSalival_Tileset2" tilewidth="64" tileheight="64" tilecount="9" columns="3">
- <image source="../../../../../../Downloads/GlandulaSalival_Tileset2.png" width="194" height="194"/>
+ <image source="GlandulaSalival_Tileset2.png" width="194" height="194"/>
 </tileset>
