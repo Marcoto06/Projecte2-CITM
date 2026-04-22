fc936207748588103f15a78d4074d48b5d943c67
HuggESC <hugo.escobar@estudiantat.upc.edu>
2026-04-22 22:55:55 +0200

    Update Atlas_Eosinofilo.tsx

--- PlatformGame-L19_Optimization/Assets/Textures/Characters/Atlas_Eosinofilo.tsx
+++ PlatformGame-L19_Optimization/Assets/Textures/Characters/Atlas_Eosinofilo.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Atlas_eosinofilo_final" tilewidth="256" tileheight="256" tilecount="312" columns="24">
- <image source="Atlas_eosinofilo_final.png" width="6144" height="3328"/>
+<tileset version="1.10" tiledversion="1.12.1" name="Atlas_eosinofilo_final" tilewidth="256" tileheight="256" tilecount="312" columns="24">
+ <image source="Atlas_Eosinofilo.png" width="6144" height="3328"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
@@ -27,6 +27,8 @@
    <frame tileid="21" duration="100"/>
    <frame tileid="22" duration="100"/>
    <frame tileid="23" duration="100"/>
+   <frame tileid="96" duration="120"/>
+   <frame tileid="96" duration="120"/>
   </animation>
  </tile>
  <tile id="24">
