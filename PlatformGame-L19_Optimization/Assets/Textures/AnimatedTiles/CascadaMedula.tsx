5a9786845e2a33a9910131e0d555255bcf69f96f
abelnunez5 <abel.nunez@estudiantat.upc.edu>
2026-06-09 19:31:22 +0200

    Added Cascadas Medula

---
+++ PlatformGame-L19_Optimization/Assets/Textures/AnimatedTiles/CascadaMedula.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="CascadaMedulaRayos" tilewidth="128" tileheight="174" tilecount="11" columns="1">
+ <image source="CascadaMedulaRayos.png" width="128" height="1915"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
